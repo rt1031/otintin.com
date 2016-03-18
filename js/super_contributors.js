5995b661b45007a9e65124e8a1664b54731bb4ff
Nakahiko <takanakahiko@gmail.com>
2016-03-19 00:30:04 +0900

    眠い

--- js/super_contributors.js
+++ js/super_contributors.js
@@ -4,3 +4,15 @@
 
 	document.getElementsByTagName('head')[0].appendChild(script);
 };
+
+function cb(json){
+	
+	var text = "";
+	
+	for(var i = 0; i < json.data.length;i++){
+		text = text + "<a href='" + json.data[i].html_url + "'>" + json.data[i].login + "</a>";
+		text = text + "<br>"
+	}
+	
+	document.getElementById("contributors").innerHTML = text;
+}
