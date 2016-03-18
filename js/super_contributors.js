0f12967f23f74fea6806d5ff77a69058a4fa8ce2
Nakahiko <takanakahiko@gmail.com>
2016-03-18 23:57:28 +0900

    準備

---
+++ js/super_contributors.js
@@ -0,0 +1,6 @@
+﻿window.onload = function() {
+ 	var script = document.createElement('script');
+	script.src = 'https://api.github.com/repositories/39287637/contributors?callback=cb';
+
+	document.getElementsByTagName('head')[0].appendChild(script);
+};
