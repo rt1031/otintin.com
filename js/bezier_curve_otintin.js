798dfaf268f06f4b2e8426b93badc13cdc6daa18
prprhyt <prprhyt@outlook.com>
2016-04-29 10:44:16 +0900

    アニメーションリセット条件の修正、名前空間への配慮、後置インクリメント

--- js/bezier_curve_otintin.js
+++ js/bezier_curve_otintin.js
@@ -3,44 +3,44 @@ window.addEventListener("DOMContentLoaded", function(){
 });
 
 function draw_bezier_curve_otintin() {//男根のメタファー
-	var canvas = document.getElementsByClassName('img_cnvs_animetion');
-	var tintin_width = new Array();
+	var otintin_canvas = document.getElementsByClassName('img_cnvs_animetion');
+	var otintin_width = new Array();
 	var img_array = new Array();
 	var loaded_img_count = 1;
-	for(var i=0;i<canvas.length;++i){
-		tintin_width[i]=0;
+	for(var i=0;i<otintin_canvas.length;++i){
+		otintin_width[i]=0;
 		img_array[i]=new Image();
-		img_array[i].src=canvas[i].dataset.imgsrc;
+		img_array[i].src=otintin_canvas[i].dataset.imgsrc;
 	}
 	var loop_animetion = function(){
 		var count_end_animetion = 0;
-		for(var i=0;i<canvas.length;++i){
-			if (canvas[i].getContext) {
-				var ctx = canvas[i].getContext('2d');
-				var standard_x=canvas[i].width/2;
-				var standard_y=canvas[i].height/2;
-				if(tintin_width[i]>canvas[i].width){
+		for(var i=0;i<otintin_canvas.length;++i){
+			if (otintin_canvas[i].getContext) {
+				var ctx = otintin_canvas[i].getContext('2d');
+				var standard_x=otintin_canvas[i].width/2;
+				var standard_y=otintin_canvas[i].height/2;
+				var otintin_height = otintin_width*(13/4);
+				if(otintin_width[i]>otintin_canvas[i].width&&otintin_height>otintin_canvas[i].height){
 					//++count_end_animetion;
-					tintin_width[i]=0;
+					otintin_width[i]=0;
 				}
-				tintin_width[i] += 30;
-				var tintin_height = tintin_width*(13/4);
+				otintin_width[i] += 30;
 				//height:width = 13:4
-				ctx.drawImage(img_array[i],0,0,canvas[i].width,canvas[i].height);
+				ctx.drawImage(img_array[i],0,0,otintin_canvas[i].width,otintin_canvas[i].height);
 				ctx.beginPath();
 				ctx.globalCompositeOperation = "destination-in";
-				ctx.moveTo(standard_x-tintin_width/2,standard_y-tintin_height*(5/26));
-				ctx.quadraticCurveTo(standard_x, standard_y-tintin_height/2, standard_x+tintin_width/2, standard_y-tintin_height*(5/26));
-				ctx.quadraticCurveTo(standard_x+tintin_width/2-tintin_width/40,standard_y-tintin_height*(4/26),standard_x+tintin_width/2-tintin_width/8,standard_y-tintin_height*(4/26));
-				ctx.quadraticCurveTo(standard_x+tintin_width/2-tintin_width/40,standard_y-tintin_height*(4/26),standard_x+tintin_width/2-tintin_width/8,standard_y+tintin_height/2);
-				ctx.lineTo(standard_x-tintin_width/2+tintin_width/8,standard_y+tintin_height/2);
-				ctx.quadraticCurveTo(standard_x-tintin_width/2+tintin_width/40,standard_y-tintin_height*(4/26),standard_x-tintin_width/2+tintin_width/8,standard_y-tintin_height*(4/26));
-				ctx.quadraticCurveTo(standard_x-tintin_width/2+tintin_width/40,standard_y-tintin_height*(4/26),standard_x-tintin_width/2,standard_y-tintin_height*(5/26));
+				ctx.moveTo(standard_x-otintin_width/2,standard_y-otintin_height*(5/26));
+				ctx.quadraticCurveTo(standard_x, standard_y-otintin_height/2, standard_x+otintin_width/2, standard_y-otintin_height*(5/26));
+				ctx.quadraticCurveTo(standard_x+otintin_width/2-otintin_width/40,standard_y-otintin_height*(4/26),standard_x+otintin_width/2-otintin_width/8,standard_y-otintin_height*(4/26));
+				ctx.quadraticCurveTo(standard_x+otintin_width/2-otintin_width/40,standard_y-otintin_height*(4/26),standard_x+otintin_width/2-otintin_width/8,standard_y+otintin_height/2);
+				ctx.lineTo(standard_x-otintin_width/2+otintin_width/8,standard_y+otintin_height/2);
+				ctx.quadraticCurveTo(standard_x-otintin_width/2+otintin_width/40,standard_y-otintin_height*(4/26),standard_x-otintin_width/2+otintin_width/8,standard_y-otintin_height*(4/26));
+				ctx.quadraticCurveTo(standard_x-otintin_width/2+otintin_width/40,standard_y-otintin_height*(4/26),standard_x-otintin_width/2,standard_y-otintin_height*(5/26));
 				ctx.fill();
 				ctx.globalCompositeOperation = "source-over";
 			}
 		}
-		if(count_end_animetion==0){
+		if(!count_end_animetion){
 			setTimeout(loop_animetion,50);
 		}
 	}
@@ -50,7 +50,7 @@ function draw_bezier_curve_otintin() {//男根のメタファー
             if (loaded_img_count == img_array.length) {
             	setTimeout(loop_animetion,50);
             }
-            loaded_img_count++;
+            ++loaded_img_count;
         }, false);
     }
 }
